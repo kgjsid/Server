080b24f86b650321a3036ed662a43a3501aa2d46
kgjsid <kgjsid@gmail.com>
2024-09-13 14:45:50 +0900

    데드락 파트 필기

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,55 +4,71 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class FastLock
+    {
+        public int id;
+    }
+
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
         static object obj = new object();
-        
 
         static void Thread_1()
         {
-            // 상호배제(Mutual Exclusive)
-
-            for(int i = 0; i < 1000000; i++)
+            for(int i = 0; i < 10000; i++)
             {
-                // 1. return에서 잠구기
-                // Monitor.Enter(obj);
-                // number++;
-                // 
-                // Monitor.Exit(obj);
-                // return;
-
-                // 2. try - finally 이용
-                // try
-                // {
-                //     Monitor.Enter(obj);
-                //     number++;
-                // 
-                //     return;
-                // }
-                // finally
-                // {
-                //     Monitor.Exit(obj);
-                // }
-
-                // 3. lock 키워드 이용
-                lock(obj)
-                {
-                    number++;
-                }
+                SessionManager.Test();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                Monitor.Enter(obj);
-
-                number--;
-
-                Monitor.Exit(obj);
+                UserManager.Test();
             }
         }
 
