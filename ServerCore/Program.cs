5e2a9dfd98216c3690430baa17b069aaf71054b8
kgjsid <kgjsid@gmail.com>
2024-09-13 16:45:04 +0900

    SpinLock 구현 연습

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,71 +4,67 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class FastLock
+    class SpinLock
     {
-        public int id;
-    }
+        // 잡혀 있는 상태의 표현
+        volatile int _locked = 0;
 
-    class SessionManager
-    {
-        static object _lock = new object();
+        public void Acquire()
+        {   // Monitor Enter의 역할(획득)
 
-        public static void TestSession()
-        {
-            lock (_lock)
+            // 레퍼런스로 변수를 하나 넣어주고, 세팅할 값을 넣어줌
+            // 최종적으로 주는 값은 이전에 원본 값을 줌
+            // => 이것이 가지는 의미가 굉장히 중요함.
+            // 만약 이전에 잠근 상태가 있었다면 1을 줄거임. 그것은 한번 더 잠궜다는 의미
+
+            while (true)
             {
+                // CompareExchange : 일반적인 버전. 
+                // (조작하기 위한 값, 넣어줄 값, 비교할 값)
+                // => 조작하기 위한 값과 비교할 값을 보고, 같다면 넣어줄 값을 넣음
+                // 이런 계열의 함수를 CAS(Compare And Swap) / C++에도 있음
+                int expected = 0;   // 예상되는 값
+                int desired = 1;    // 넣고싶은 값
 
+                if(Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
+                {
+                    break;
+                }
             }
         }
 
-        public static void Test()
-        {
-            lock(_lock)
-            {
-                UserManager.TestUser();
-            }
-        }
-    }
-
-    class UserManager
-    {
-        static object _lock = new object();
-
-        public static void Test()
-        {
-            lock(_lock)
-            {
-                SessionManager.TestSession();
-            }
-        }
-
-        public static void TestUser()
-        {
-            lock(_lock)
-            {
-
-            }
+        public void Release()
+        {   // Monitor Exit의 역할(내려놓기)
+            _locked = 0;
         }
     }
 
     class Program
     {
         static int number = 0;
-        static object obj = new object();
+        static SpinLock _lock = new SpinLock();
 
         static void Thread_1()
         {
-            for(int i = 0; i < 10000; i++)
+            for(int i = 0; i < 100000; i++)
             {
-                SessionManager.Test();
+                _lock.Acquire();
+
+                number++;
+
+                _lock.Release();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                UserManager.Test();
+                _lock.Acquire();
+
+                number--;
+
+                _lock.Release();
             }
         }
 
