71902d165aaad339d226ccd5e923ca5e040fb253
kgjsid <kgjsid@gmail.com>
2024-09-13 11:34:22 +0900

    메모리 배리어 파트 공부

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,46 +6,29 @@ namespace ServerCore
 {
     class Program
     {
-        // volatile : 휘발성
-        // -> stop은 휘발성 데이터(언제 변할지 모름 / 최적화하지 마라)
-        volatile static bool stop = false;
+        int answer;
+        bool complete;
 
-        static void ThreadMain()
-        {
-            Console.WriteLine("스레드 시작!");
-
-            // 최적화를 위해 코드를 분석하다 보니까
-            // 코드를 수정함. => 내부에서 stop이 수정없으니 로직은 동일하게 하므로
-            if (stop == false)
-            {
-                while(true)
-                {
-                    // stop이 false이면 무한 루프
-                }
-            }
-
-            while(stop == false)
-            {
-                // stop이 false이면 무한 루프
-            }
-
-            Console.WriteLine("스레드 종료!");
+        void A()
+        {   // Write만 하는 스레드
+            answer = 123;
+            Thread.MemoryBarrier(); // Barrier 1
+            complete = true;
+            Thread.MemoryBarrier(); // Barrier 2
+            // 마지막에 넣는 이유? -> Write를 할 때마다 써줌
+            // -> 확실히 가시성을 챙기기 위함
         }
 
-        static void Main(string[] args)
-        {
-            Task t = new Task(ThreadMain);
-            t.Start();
-
-            // Sleep(1000)? : 해당 스레드가 잠시 1초 멈춤
-            Thread.Sleep(1000);
-            stop = true;
-
-            Console.WriteLine("Stop 변경");
-            Console.WriteLine("종료 대기중");
-            // Wait : Thread의 Join과 동일한 역할
-            t.Wait();   
-            Console.WriteLine("종료 성공");
+        void B()
+        {   // Read만 하는 스레드
+            // 처음에 넣는 이유? -> Read를 하기 전 써줌
+            // -> 또한 확실히 가시성을 챙기기 위함
+            Thread.MemoryBarrier(); // Barrier 3
+            if(complete)
+            {
+                Thread.MemoryBarrier(); // Barrier 4
+                Console.WriteLine(answer);
+            }
         }
     }
 }
