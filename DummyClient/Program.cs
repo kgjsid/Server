758304c7465a9fc8ca936d57416adef6ad293fc2
kgjsid <kgjsid@gmail.com>
2024-09-19 17:11:41 +0900

    소켓 프로그래밍 연습

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace DummyClient
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string host = Dns.GetHostName();
+            Console.WriteLine(host);
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];               // ip 주소
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 소켓 설정(휴대전화)
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 입장 가능 여부 확인
+                socket.Connect(endPoint);
+
+                // RemoteEndPoint : 연결된 서버라고 생각
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World");
+                int sendBytes = socket.Send(sendBuff);
+
+                // 받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+
+                Console.WriteLine($"[From Server] {recvData}");
+
+                // 나가기
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString()); ;
+            }
         }
     }
 }
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,39 +1,71 @@
-﻿
-using System.Runtime.CompilerServices;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore
 {
     class Program
     {
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
-
-        static void WhoAmI()
-        {   // ThreadLocal.Value를 통해 값에 접근 및 수정 가능
-            // ThreadName.Value = $"My Name Is {Thread.CurrentThread.ManagedThreadId}";
-
-            // IsValueCreate : 이미 만들어져 있다면 true로 할당
-            bool repeat = ThreadName.IsValueCreated;
-
-            Thread.Sleep(1000);
-
-            if (repeat)
-            {
-                Console.WriteLine($"{ThreadName.Value} (repeat)");
-            }
-            else
-                Console.WriteLine($"{ThreadName.Value}");
-        }
-
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(2, 2);
+            // DNS(Domain Name System)
+            // IP주소를 매칭하는 이름이라고 생각
+            // 지금 현재 컴퓨터로 서버를 동작시키니 서버 컴퓨터의 IP주소가 필요한 상황
+            string host = Dns.GetHostName();
+            Console.WriteLine(host);
+            IPHostEntry ipHost = Dns.GetHostEntry(host);            
+            IPAddress ipAddr = ipHost.AddressList[0];               // ip 주소
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);     // 최종 주소(ip, 포트 번호)
 
-            // Invoke에 넣어주는 액션 만큼을 태스크로 만들어 실행
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // ipHost.AddressList -> 원하는 네트워크 주소(배열값)
+            // 구글같이 트래픽이 많은 경우 부하 분산을 위해 DNS가 여러 주소를 가지는 경우도 많음
+            // 포트 번호 : 정문, 후문 생각 -> 클라이언트가 엉뚱한 번호로 접근하면 못하도록 막음
+            // 클라이언트가 접속할 주소도 똑같이 맞춰줘야 함
 
-            // 필요없다면 날려주는 메소드
-            ThreadName.Dispose();
+            // 리슨 소켓 제작
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // bind 작업(문지기 교육)
+                listenSocket.Bind(endPoint);
+
+                // Listen(영업 시작)
+                // backLog : 최대 대기 수
+                listenSocket.Listen(10);
+
+                while (true)
+                {   // 루프를 돌면서 체크
+                    Console.WriteLine("Listening...");
+
+                    // 클라이언트(손님)을 입장 -> Accept
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // 받기
+                    // -> 클라이언트가 보내는 정보를 받기 위함
+                    // recvBuff에 클라이언트가 보내준 데이터가, recvBytes에는 몇 바이트를 받았는지
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    // 문자로 테스트를 위해 작성
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 보내기
+                    // -> 클라이언트에게 메시지 전송
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+                    clientSocket.Send(sendBuff);
+
+                    // 쫓아낸다
+                    // -> 예고를 하고 쫓아냄
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
         }
     }
 }
