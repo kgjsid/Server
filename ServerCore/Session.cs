85a9363bc2789d43d78759b0629d6a941adff26f
kgjsid <kgjsid@gmail.com>
2024-09-20 14:27:48 +0900

    SendAsync 연습

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -7,25 +7,38 @@ namespace ServerCore
     {
         Socket clientSocket;
         int _disconnected = 0;
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
+        object _lock = new object();
+        bool _pending = false;
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
 
         public void Start(Socket clientSocket)
         {
             this.clientSocket = clientSocket;
 
             SocketAsyncEventArgs recvargs = new SocketAsyncEventArgs();
-            recvargs.Completed += new EventHandler<SocketAsyncEventArgs>(OnResisterCompleted);
+            recvargs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
 
             // receive의 경우 버퍼를 통하여 데이터를 받아오므로
             // 버퍼 설정이 필요함
             // 추가적인 정보가 필요할 때 recvargs.UserToken = this;
             recvargs.SetBuffer(new byte[1024], 0, 1024);
 
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
+
             ResisterRecv(recvargs);
         }
 
         public void Send(byte[] sendBuff)
         {
-            clientSocket.Send(sendBuff);
+            lock (_lock)
+            {   
+                _sendQueue.Enqueue(sendBuff);
+
+                if (_pending == false)
+                    ResisterSend();
+            }
         }
 
         public void Disconnect()
@@ -39,17 +52,62 @@ namespace ServerCore
         }
 
         #region 네트워크 통신(Recv)
+
+        void ResisterSend()
+        {   // Send 등록
+            _pending = true;    // 현재 하나의 Send 진행 중
+            byte[] buff = _sendQueue.Dequeue();
+            _sendArgs.SetBuffer(buff, 0, buff.Length);
+
+            bool pending = clientSocket.SendAsync(_sendArgs);
+
+            if (pending == false)
+                OnSendCompleted(null, _sendArgs);
+        }
+
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {   // Send 처리
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {   // Send 진행 종료
+
+                        if (_sendQueue.Count > 0)
+                        {   // pending 중인 상황에서 누군가 Send를 또 하게되면
+                            // Send에 대한 등록, 처리가 이루어지지 않고 큐에만 데이터가 들어감
+                            // 추가 처리가 필요함
+                            ResisterSend();
+                        }
+                        else
+                        {   // 그 사이에 아무도 패킷을 추가하지 않음
+                            _pending = false;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Failed {e.ToString()}");
+                    }
+                }
+                else
+                {
+                    Disconnect();
+                }
+            }
+        }
+
         void ResisterRecv(SocketAsyncEventArgs args)
         {   // receive 요청
             bool pending = clientSocket.ReceiveAsync(args);
 
             if(pending == false)
             {
-                OnResisterCompleted(null, args);
+                OnRecvCompleted(null, args);
             }
         }
 
-        void OnResisterCompleted(object sender, SocketAsyncEventArgs args)
+        void OnRecvCompleted(object sender, SocketAsyncEventArgs args)
         {   // receive 처리
             if(args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
             {   // 상대방이 내용을 보내고 성공했다면
@@ -68,7 +126,7 @@ namespace ServerCore
             }
             else
             {
-
+                Disconnect();
             }
         }
         #endregion
